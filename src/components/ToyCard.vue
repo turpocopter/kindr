91d6dcd15837a5e54d3fbef5704e5fba525a6809
Yann Jacquot <yann.jacquot@recommerce.com>
2026-03-26 12:16:40 +0100

    Remove image name from toy card

--- src/components/ToyCard.vue
+++ src/components/ToyCard.vue
@@ -16,10 +16,5 @@ defineProps<{
       class="h-full w-full object-cover pointer-events-none"
       draggable="false"
     />
-    <div
-      class="absolute inset-x-0 bottom-0 bg-black/45 px-4 py-4 text-white backdrop-blur-sm"
-    >
-      <h3 class="text-2xl font-bold">{{ toy.name }}</h3>
-    </div>
   </article>
 </template>
